7fc0f2e9ae81ce97b2a4b2cb8238f1e6ab2bcd92
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-06 22:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function toCase(text) {
   // write your code here
+    if (text.length === 0) {
+        return '-';
+    }
+
+    const lowerCasetext = text.toLowerCase();
+    const upperCasetext= text.toUpperCase();
+
+    return `${lowerCasetext}-${upperCasetext}`;
 }
 
 // DO not change the code below
